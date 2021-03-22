dfe3b7d89d60e3dca3062d4ddb385f6693b0a725
Pedro <pedro.moreira01@fatec.sp.gov.br>
2021-03-21 21:40:09 -0300

    WIP: e-mail backup feature.

---
+++ common/src/main/java/com/github/pedroarrudamoreira/vaultage/util/EasyUnzip.java
@@ -0,0 +1,56 @@
+package com.github.pedroarrudamoreira.vaultage.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class EasyUnzip {
+
+	public static void extract(File destDir, InputStream is) throws IOException, FileNotFoundException {
+		ZipInputStream zis = new ZipInputStream(is);
+		byte[] buffer = new byte[1024];
+		ZipEntry zipEntry;
+		while ((zipEntry = zis.getNextEntry()) != null) {
+			File newFile = newFile(destDir, zipEntry);
+			if (zipEntry.isDirectory()) {
+				if (!newFile.isDirectory() && !newFile.mkdirs()) {
+					throw new IOException("Failed to create directory " + newFile);
+				}
+			} else {
+				// fix for Windows-created archives
+				File parent = newFile.getParentFile();
+				if (!parent.isDirectory() && !parent.mkdirs()) {
+					throw new IOException("Failed to create directory " + parent);
+				}
+
+				// write file content
+				FileOutputStream fos = new FileOutputStream(newFile);
+				int len;
+				while ((len = zis.read(buffer)) > 0) {
+					fos.write(buffer, 0, len);
+				}
+				fos.close();
+			}
+			zis.closeEntry();
+		}
+		zis.close();
+	}
+
+	private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+		File destFile = new File(destinationDir, zipEntry.getName());
+
+		String destDirPath = destinationDir.getCanonicalPath();
+		String destFilePath = destFile.getCanonicalPath();
+
+		if (!destFilePath.startsWith(destDirPath + File.separator)) {
+			throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+		}
+
+		return destFile;
+	}
+
+}
